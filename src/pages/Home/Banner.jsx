bc02f313cee8e0e4481a9c772d903920312da9f0
kawsar12759 <kawsar15-12759@diu.edu.bd>
2024-09-08 20:44:35 +0600

    worked on navbar and home page banner

---
+++ src/pages/Home/Banner.jsx
@@ -0,0 +1,92 @@
+import carouselImg1 from '../../assets/carouselImg1.jpg';
+import carouselImg2 from '../../assets/carouselImg2.jpg';
+import carouselImg3 from '../../assets/carouselImg3.jpg';
+import carouselImg4 from '../../assets/carouselImg4.png';
+
+const Banner = () => {
+    return (
+        <div>
+            <div className="carousel w-full">
+                <div id="slide1" className="carousel-item relative w-full">
+                    <img style={{ height: '700px' }}
+                        src={carouselImg1}
+                        className="w-full" />
+                    <div className="absolute inset-x-0 bottom-0 flex flex-col justify-end px-16 md:px-32 lg:px-48 py-8 bg-gradient-to-b from-transparent to-gray-900 bg-opacity-75">
+                        <h2 className="text-4xl text-white font-bold text-center mb-5">Find Your Perfect Home with NestVibes</h2>
+                        <p className="text-lg text-gray-200 mt-2 text-center mb-4">
+                            Discover a world of exceptional living spaces tailored to your needs. Whether you're searching for a cozy apartment, a spacious family home, or a charming vacation rental, NestVibes is here to help you find the ideal place. Explore our diverse property listings, connect with us, and start your journey to finding a home that truly feels like yours.
+                        </p>
+                        <div className='flex justify-center'>
+                            <a href="/brochure" className="bg-teal-500 text-white py-2 px-4 rounded hover:bg-teal-600">View Listing</a>
+                            <a href="/contact" className=" ml-4 bg-yellow-500 text-black py-2 px-4 rounded hover:bg-yellow-600">Contact Us</a>
+                        </div>
+                    </div>
+                    <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
+                        <a href="#slide4" className="btn btn-circle">❮</a>
+                        <a href="#slide2" className="btn btn-circle">❯</a>
+                    </div>
+                </div>
+                <div id="slide2" className="carousel-item relative w-full">
+                    <img style={{ height: '700px' }}
+                        src={carouselImg2}
+                        className="w-full" />
+                    <div className="absolute inset-x-0 bottom-0 flex flex-col justify-end px-16 md:px-32 lg:px-48 py-8 bg-gradient-to-b from-transparent to-gray-900 bg-opacity-75">
+                        <h2 className="text-4xl text-white font-bold text-center mb-5">Find Your Perfect Home with NestVibes</h2>
+                        <p className="text-lg text-gray-200 mt-2 text-center mb-4">
+                            Discover a world of exceptional living spaces tailored to your needs. Whether you're searching for a cozy apartment, a spacious family home, or a charming vacation rental, NestVibes is here to help you find the ideal place. Explore our diverse property listings, connect with us, and start your journey to finding a home that truly feels like yours.
+                        </p>
+                        <div className='flex justify-center'>
+                            <a href="/brochure" className="bg-teal-500 text-white py-2 px-4 rounded hover:bg-teal-600">View Listing</a>
+                            <a href="/contact" className=" ml-4 bg-yellow-500 text-black py-2 px-4 rounded hover:bg-yellow-600">Contact Us</a>
+                        </div>
+                    </div>
+                    <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
+                        <a href="#slide1" className="btn btn-circle">❮</a>
+                        <a href="#slide3" className="btn btn-circle">❯</a>
+                    </div>
+                </div>
+                <div id="slide3" className="carousel-item relative w-full">
+                    <img style={{ height: '700px' }}
+                        src={carouselImg3}
+                        className="w-full" />
+                    <div className="absolute inset-x-0 bottom-0 flex flex-col justify-end px-16 md:px-32 lg:px-48 py-8 bg-gradient-to-b from-transparent to-gray-900 bg-opacity-75">
+                        <h2 className="text-4xl text-white font-bold text-center mb-5">Find Your Perfect Home with NestVibes</h2>
+                        <p className="text-lg text-gray-200 mt-2 text-center mb-4">
+                            Discover a world of exceptional living spaces tailored to your needs. Whether you're searching for a cozy apartment, a spacious family home, or a charming vacation rental, NestVibes is here to help you find the ideal place. Explore our diverse property listings, connect with us, and start your journey to finding a home that truly feels like yours.
+                        </p>
+                        <div className='flex justify-center'>
+                            <a href="/brochure" className="bg-teal-500 text-white py-2 px-4 rounded hover:bg-teal-600">View Listing</a>
+                            <a href="/contact" className=" ml-4 bg-yellow-500 text-black py-2 px-4 rounded hover:bg-yellow-600">Contact Us</a>
+                        </div>
+
+                    </div>
+                    <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
+                        <a href="#slide2" className="btn btn-circle">❮</a>
+                        <a href="#slide4" className="btn btn-circle">❯</a>
+                    </div>
+                </div>
+                <div id="slide4" className="carousel-item relative w-full">
+                    <img style={{ height: '700px' }}
+                        src={carouselImg4}
+                        className="w-full" />
+                    <div className="absolute inset-x-0 bottom-0 flex flex-col justify-end px-16 md:px-32 lg:px-48 py-8 bg-gradient-to-b from-transparent to-gray-900 bg-opacity-75">
+                        <h2 className="text-4xl text-white font-bold text-center mb-5">Find Your Perfect Home with NestVibes</h2>
+                        <p className="text-lg text-gray-200 mt-2 text-center mb-4">
+                            Discover a world of exceptional living spaces tailored to your needs. Whether you're searching for a cozy apartment, a spacious family home, or a charming vacation rental, NestVibes is here to help you find the ideal place. Explore our diverse property listings, connect with us, and start your journey to finding a home that truly feels like yours.
+                        </p>
+                        <div className='flex justify-center'>
+                            <a href="/brochure" className="bg-teal-500 text-white py-2 px-4 rounded hover:bg-teal-600">View Listing</a>
+                            <a href="/contact" className=" ml-4 bg-yellow-500 text-black py-2 px-4 rounded hover:bg-yellow-600">Contact Us</a>
+                        </div>
+                    </div>
+                    <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
+                        <a href="#slide3" className="btn btn-circle">❮</a>
+                        <a href="#slide1" className="btn btn-circle">❯</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Banner;
--- src/shared/Navbar/Navbar.jsx
+++ src/shared/Navbar/Navbar.jsx
@@ -1,9 +1,19 @@
+import { Link, NavLink } from "react-router-dom";
 
 
 const Navbar = () => {
+
+    const navLinks = <>
+        <li><NavLink className={({ isActive }) => isActive ? "!text-[#0D9488] !bg-[#F5F5F5] hover:bg-[#F5F5F5] hover:text-[#0D9488] active:!bg-[#004D40] active:!text-[#FFFFFF]  visited:!bg-[#F5F5F5] visited:!text-[#0D9488]" : "active:!bg-[#004D40] active:!text-[#FFFFFF]"} to='/'>Home</NavLink></li>
+        <li><NavLink className={({ isActive }) => isActive ? "!text-[#0D9488] !bg-[#F5F5F5] hover:bg-[#F5F5F5] hover:text-[#0D9488] active:!bg-[#004D40] active:!text-[#FFFFFF]  visited:!bg-[#F5F5F5] visited:!text-[#0D9488]" : "active:!bg-[#004D40] active:!text-[#FFFFFF]"} to='/about'>About Us</NavLink></li>
+        <li><NavLink className={({ isActive }) => isActive ? "!text-[#0D9488] !bg-[#F5F5F5] hover:bg-[#F5F5F5] hover:text-[#0D9488] active:!bg-[#004D40] active:!text-[#FFFFFF]  visited:!bg-[#F5F5F5] visited:!text-[#0D9488]" : "active:!bg-[#004D40] active:!text-[#FFFFFF]"} to='/blog'>Blog</NavLink></li>
+        <li><NavLink className={({ isActive }) => isActive ? "!text-[#0D9488] !bg-[#F5F5F5] hover:bg-[#F5F5F5] hover:text-[#0D9488] active:!bg-[#004D40] active:!text-[#FFFFFF]  visited:!bg-[#F5F5F5] visited:!text-[#0D9488]" : "active:!bg-[#004D40] active:!text-[#FFFFFF]"} to='/reviews'>Reviews</NavLink></li>
+        <li><NavLink className={({ isActive }) => isActive ? "!text-[#0D9488] !bg-[#F5F5F5] hover:bg-[#F5F5F5] hover:text-[#0D9488] active:!bg-[#004D40] active:!text-[#FFFFFF]  visited:!bg-[#F5F5F5] visited:!text-[#0D9488]" : "active:!bg-[#004D40] active:!text-[#FFFFFF]"} to='/updateprofile'>Update Profile</NavLink></li>
+    </>
+
     return (
-        <div>
-            <div className="navbar bg-base-100">
+        <div className="bg-[#0D9488] text-[#E5E7EB] px-16 py-2">
+            <div className="navbar">
                 <div className="navbar-start">
                     <div className="dropdown">
                         <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
@@ -22,37 +32,27 @@ const Navbar = () => {
                         </div>
                         <ul
                             tabIndex={0}
-                            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
-                            <li><a>Item 1</a></li>
-                            <li>
-                                <a>Parent</a>
-                                <ul className="p-2">
-                                    <li><a>Submenu 1</a></li>
-                                    <li><a>Submenu 2</a></li>
-                                </ul>
-                            </li>
-                            <li><a>Item 3</a></li>
+                            className="menu menu-sm dropdown-content bg-base-100 text-[#0D9488] rounded-box z-[1] mt-3 w-52 p-2 shadow">
+                            {navLinks}
                         </ul>
                     </div>
-                    <a className="btn btn-ghost text-xl">daisyUI</a>
+                    <Link className="btn btn-ghost text-2xl">NestVibes</Link>
                 </div>
                 <div className="navbar-center hidden lg:flex">
-                    <ul className="menu menu-horizontal px-1">
-                        <li><a>Item 1</a></li>
-                        <li>
-                            <details>
-                                <summary>Parent</summary>
-                                <ul className="p-2">
-                                    <li><a>Submenu 1</a></li>
-                                    <li><a>Submenu 2</a></li>
-                                </ul>
-                            </details>
-                        </li>
-                        <li><a>Item 3</a></li>
+                    <ul className="menu menu-horizontal px-1 font-medium">
+                        {navLinks}
                     </ul>
                 </div>
                 <div className="navbar-end">
-                    <a className="btn">Button</a>
+                    <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
+                        <div className="w-12 rounded-full">
+                            <img
+                                alt="Tailwind CSS Navbar component"
+                                src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
+                        </div>
+                    </div>
+                    <Link><button className="bg-[#4CAF50] text-[#FFFFFF] px-6 py-3 rounded-md mr-5 font-semibold">Sign In</button></Link>
+                    <Link className="bg-[#FF5722] text-[#FFFFFF] px-6 py-3 rounded-md font-semibold"><button>Sign Up</button></Link>
                 </div>
             </div>
         </div>
